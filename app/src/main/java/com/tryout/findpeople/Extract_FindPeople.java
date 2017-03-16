8ae8c2fc839c3f34c459b4767045f8e2cb30f30a
Kshitij Jain <jainkshitij16@outlook.com>
2017-03-16 12:13:33 -0700

    Added Referance class

---
+++ app/src/main/java/com/tryout/findpeople/Extract_FindPeople.java
@@ -0,0 +1,14 @@
+package com.tryout.findpeople;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Kshitij on 2017-03-16.
+ */
+public class Extract_FindPeople extends AsyncTask<Void,Void,String> {
+
+    @Override
+    protected String doInBackground(Void... params) {
+        return null;
+    }
+}
