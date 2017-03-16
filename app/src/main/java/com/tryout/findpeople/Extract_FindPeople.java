8a95afd99ec8579bf3bb94e141a4a8466c5206c5
Kshitij Jain <jainkshitij16@outlook.com>
2017-03-16 13:17:40 -0700

    cleaned code

--- app/src/main/java/com/tryout/findpeople/Extract_FindPeople.java
+++ app/src/main/java/com/tryout/findpeople/Extract_FindPeople.java
@@ -6,7 +6,19 @@ import android.os.AsyncTask;
  * Created by Kshitij on 2017-03-16.
  */
 public class Extract_FindPeople extends AsyncTask<Void,Void,String> {
+/*
+    AsyncTask class requires following methods
+    Params; type of parameter sent to the task upon execution
+    Progress; type of progress units to perform in the background
+    Result; type of the result of the background execution
 
+    *Use Void if one of them don't have any primitive or objects*
+ */
+
+    /*
+        Parameter matches with Params
+        Method states what processes to run in the background
+     */
     @Override
     protected String doInBackground(Void... params) {
         return null;
