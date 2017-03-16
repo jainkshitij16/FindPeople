6291db897d4c21e87f9c5037a25c2470405a264f
Kshitij Jain <jainkshitij16@outlook.com>
2017-03-16 01:40:22 -0700

    Changed layout

--- app/src/main/java/com/tryout/findpeople/MainActivity_FindPeople.java
+++ app/src/main/java/com/tryout/findpeople/MainActivity_FindPeople.java
@@ -2,12 +2,38 @@ package com.tryout.findpeople;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity_FindPeople extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    ProgressBar progressBar;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity__find_people);
+
+        editText = (EditText) findViewById(R.id.edit_emailtext);
+        button = (Button) findViewById(R.id.button_search);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        textView = (TextView) findViewById(R.id.text_response);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+
     }
 }
+
+
