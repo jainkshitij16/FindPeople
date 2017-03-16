ffd71ec16d7900ebe5f611d6a9de6c501bb4b178
Kshitij Jain <jainkshitij@outlook.com>
2017-03-15 17:20:19 -0700

    initial commit

---
+++ app/src/main/java/com/tryout/findpeople/MainActivity_FindPeople.java
@@ -0,0 +1,13 @@
+package com.tryout.findpeople;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_FindPeople extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity__find_people);
+    }
+}
