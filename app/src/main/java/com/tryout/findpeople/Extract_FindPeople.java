47f3ca426f432896e9ff0d26996904a79ee6ced7
Kshitij Jain <jainkshitij16@outlook.com>
2017-03-17 00:36:03 -0700

    Set up doInBackground method for the API request

--- app/src/main/java/com/tryout/findpeople/Extract_FindPeople.java
+++ app/src/main/java/com/tryout/findpeople/Extract_FindPeople.java
@@ -1,11 +1,23 @@
 package com.tryout.findpeople;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by Kshitij on 2017-03-16.
  */
 public class Extract_FindPeople extends AsyncTask<Void,Void,String> {
+    private static final String API_KEY = "&apiKey=86bc2cd8905ee179";
+    private static final String API_URL = "https://api.fullcontact.com/v2/person.json?";
+    private static final String API_EMAIL_ = "email="; // Look up by email
+    private static final String API_PHONE = "phone="; // Look up by phone
+    MainActivity_FindPeople findPeople = new MainActivity_FindPeople();
+    String user_email = findPeople.editText.toString();
 /*
     AsyncTask class requires following methods
     Params; type of parameter sent to the task upon execution
@@ -20,7 +32,28 @@ public class Extract_FindPeople extends AsyncTask<Void,Void,String> {
         Method states what processes to run in the background
      */
     @Override
-    protected String doInBackground(Void... params) {
+    protected String doInBackground(Void... urls) {
+        try{
+            // Making a call to the API and lookup using email
+            URL url = new URL(API_URL+API_EMAIL_+user_email+API_KEY);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            try{
+                // Standard process for android for handling https request.
+                // Look at Git-hub for improved process
+                InputStream inputStream = new BufferedInputStream(httpURLConnection.getInputStream());
+                readStream(inputStream);
+            }
+            finally {
+                httpURLConnection.disconnect();
+            }
+        } catch (Exception e){
+            Log.e(e.getMessage(),"URL not found");
+        }
+        return null;
+    }
+
+    // Converts InputStream to a string for doInBackground
+    private String readStream(InputStream inputStream){
         return null;
     }
 }
